e275ce36aec098060b7ff447d1dea3e86ed20146
Perry Devetzis <perryd@athenatech.ca>
2017-05-30 11:21:03 -0600

    Limit queue size. With tests

--- index.js
+++ index.js
@@ -3,46 +3,50 @@
 // Simple FIFO queue implementation to avoid having to do shift()
 // on an array, which is slow.
 
-function Queue() {
-  this.length = 0;
+function Queue(max_length) {
+    this.length = 0;
+    this.max_length = max_length || 0;
 }
 
 Queue.prototype.push = function (item) {
-  var node = {item: item};
-  if (this.last) {
-    this.last = this.last.next = node;
-  } else {
-    this.last = this.first = node;
-  }
-  this.length++;
+    var node = {item: item};
+    if (this.last) {
+        this.last = this.last.next = node;
+    } else {
+        this.last = this.first = node;
+    }
+    this.length++;
+    if (this.max_length > 0 && this.length > this.max_length) {
+        this.shift();
+    }
 };
 
 Queue.prototype.shift = function () {
-  var node = this.first;
-  if (node) {
-    this.first = node.next;
-    if (!(--this.length)) {
-      this.last = undefined;
+    var node = this.first;
+    if (node) {
+        this.first = node.next;
+        if (!(--this.length)) {
+            this.last = undefined;
+        }
+        return node.item;
     }
-    return node.item;
-  }
 };
 
 Queue.prototype.slice = function (start, end) {
-  start = typeof start === 'undefined' ? 0 : start;
-  end = typeof end === 'undefined' ? Infinity : end;
+    start = typeof start === 'undefined' ? 0 : start;
+    end = typeof end === 'undefined' ? Infinity : end;
 
-  var output = [];
+    var output = [];
 
-  var i = 0;
-  for (var node = this.first; node; node = node.next) {
-    if (--end < 0) {
-      break;
-    } else if (++i > start) {
-      output.push(node.item);
+    var i = 0;
+    for (var node = this.first; node; node = node.next) {
+        if (--end < 0) {
+            break;
+        } else if (++i > start) {
+            output.push(node.item);
+        }
     }
-  }
-  return output;
+    return output;
 }
 
 module.exports = Queue;
--- test.js
+++ test.js
@@ -4,40 +4,62 @@ var Queue = require('./');
 var test = require('tape');
 
 function init(queue) {
-  queue.push(1);
-  queue.push(2);
-  queue.push(3);
-  queue.push(4);
-  return queue;
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    queue.push(4);
+    return queue;
 }
 
 test('slice test', function (t) {
 
-  var queues = [new Queue(), new Array()];
+    var queues = [new Queue(), new Array()];
 
-  queues.forEach(function (queue) {
-    init(queue);
+    queues.forEach(function (queue) {
+        init(queue);
 
-    t.deepEqual(queue.slice(), [1, 2, 3, 4]);
-    t.deepEqual(queue.slice(2), [3, 4]);
-    t.deepEqual(queue.slice(1), [2, 3, 4]);
-    t.deepEqual(queue.slice(3), [4]);
-    t.deepEqual(queue.slice(4), []);
-    t.deepEqual(queue.slice(5), []);
-    t.deepEqual(queue.slice(0, 1), [1]);
-    t.deepEqual(queue.slice(0, 2), [1, 2]);
-    t.deepEqual(queue.slice(0, 3), [1, 2, 3]);
-    t.deepEqual(queue.slice(0, 4), [1, 2, 3, 4]);
-    t.deepEqual(queue.slice(0, 5), [1, 2, 3, 4]);
-    t.deepEqual(queue.slice(1, 2), [2]);
-    t.deepEqual(queue.slice(1, 3), [2, 3]);
-    t.deepEqual(queue.slice(1, 4), [2, 3, 4]);
-    t.deepEqual(queue.slice(2, 3), [3]);
-    t.deepEqual(queue.slice(2, 4), [3, 4]);
-    t.deepEqual(queue.slice(2, 5), [3, 4]);
-  });
+        t.deepEqual(queue.slice(), [1, 2, 3, 4]);
+        t.deepEqual(queue.slice(2), [3, 4]);
+        t.deepEqual(queue.slice(1), [2, 3, 4]);
+        t.deepEqual(queue.slice(3), [4]);
+        t.deepEqual(queue.slice(4), []);
+        t.deepEqual(queue.slice(5), []);
+        t.deepEqual(queue.slice(0, 1), [1]);
+        t.deepEqual(queue.slice(0, 2), [1, 2]);
+        t.deepEqual(queue.slice(0, 3), [1, 2, 3]);
+        t.deepEqual(queue.slice(0, 4), [1, 2, 3, 4]);
+        t.deepEqual(queue.slice(0, 5), [1, 2, 3, 4]);
+        t.deepEqual(queue.slice(1, 2), [2]);
+        t.deepEqual(queue.slice(1, 3), [2, 3]);
+        t.deepEqual(queue.slice(1, 4), [2, 3, 4]);
+        t.deepEqual(queue.slice(2, 3), [3]);
+        t.deepEqual(queue.slice(2, 4), [3, 4]);
+        t.deepEqual(queue.slice(2, 5), [3, 4]);
+    });
 
-  t.end();
+    t.end();
+
+});
+
+test('limit test', function (t) {
+
+    var queue = new Queue(5);
+
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    queue.push(4);
+    queue.push(5);
+    queue.push(6);
+    queue.push(7);
+
+    t.deepEqual(queue.slice(), [3, 4, 5, 6, 7]);
+    t.deepEqual(queue.slice(2), [5, 6, 7]);
+    t.deepEqual(queue.slice(1), [4, 5, 6, 7]);
+    t.deepEqual(queue.slice(1, 2), [4]);
+    t.deepEqual(queue.slice(2, 5), [5, 6, 7]);
+
+    t.end();
 
 });
 
